f416c9bad47ed06c77fd6d415cd7fd4cd21df497
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-02-26 18:07:21 -0400

    Changed atributes to receive account_deposit and account_withdraw

--- lib/rlsppay/account/transaction.ex
+++
@@ -1,8 +0,0 @@
-defmodule Rlsppay.Accounts.Transaction do
-
-  alias Ecto.Multi
-
-  def call("from" => from_id, "to" => to_id, "value" => value) do
-    Multi.new
-  end
-end
--- lib/rlsppay/accounts/operation.ex
+++ lib/rlsppay/accounts/operation.ex
@@ -3,10 +3,14 @@ defmodule Rslppay.Accounts.Operation do
   alias Ecto.Multi
   alias Rlsppay.{Account}
 
-  def call(%{"id"=> id, "value" => value}, operation) do
+  def call(%{"id" => id, "value" => value}, operation) do
+    operation_name = account_operation_name(operation)
+
     Multi.new()
-      |> Multi.run(:account, fn repo, _changes -> get_account(repo, id) end)
-      |> Multi.run(:update_balance, fn repo, %{account: account} -> update_balance(repo, account, value, operation) end)
+      |> Multi.run(operation_name, fn repo, _changes -> get_account(repo, id) end)
+      |> Multi.run(operation, fn repo, changes ->
+          account = Map.get(changes, operation_name)
+          update_balance(repo, account, value, operation) end)
   end
 
   defp get_account(repo, id) do
@@ -42,5 +46,9 @@ defmodule Rslppay.Accounts.Operation do
       |> repo.update()
   end
 
+  defp account_operation_name(operation) do
+    "account_#{Atom.to_string(operation)}"
+      |> String.to_atom()
+  end
 
 end
