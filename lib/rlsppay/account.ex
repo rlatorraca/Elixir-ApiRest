ec476b683c352b18e0cf0bf4043037d443e25362
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-02-26 00:29:23 -0400

    Users_view modified to show user+account created; Account model created and users/create.ex modified to work using Ecto.Multi

---
+++ lib/rlsppay/account.ex
@@ -0,0 +1,28 @@
+defmodule Rlsppay.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Rlsppay.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @required_params [:balance, :user_id]
+
+  schema "accounts" do
+    field :balance, :decimal
+    belongs_to :user, User
+    timestamps()
+  end
+
+  # faz as validacoes na insercao no DB
+  # mapeia dos dados
+  def changeset(params) do
+    %__MODULE__{}
+      |> cast(params, @required_params)
+      |> validate_required(@required_params)
+      |> check_constraint(:balance, name: :balance_must_be_positve_or_zero)
+  end
+
+
+end
--- lib/rlsppay/users/create.ex
+++ lib/rlsppay/users/create.ex
@@ -1,9 +1,46 @@
 defmodule Rlsppay.Users.Create do
-  alias Rlsppay.{Repo, User}
+  alias Rlsppay.{Account, Repo, User}
+  alias Ecto.Multi
 
   def call(params) do
+    Multi.new()
+      |> Multi.insert(:create_user, User.changeset(params))
+      |> Multi.run(:create_account,
+          fn repo, %{create_user: user} ->
+            insert_account(repo, user)
+          end)
+      |> Multi.run(:preaload_data,
+          fn repo, %{create_user: user} ->
+            preaload_data(repo, user)
+          end)
+      |> run_transaction()
+    """
     params
       |> User.changeset()
       |> Repo.insert()
+    """
+  end
+
+  defp insert_account(repo,user) do
+    user.id
+      |> account_changeset()
+      |> repo.insert()
+  end
+
+  defp preaload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp account_changeset(user_id) do
+    params = %{user_id: user_id, balance: "0.00"}
+
+    Account.changeset(params)
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
   end
 end
