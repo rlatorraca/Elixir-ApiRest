23469c0325a2023b8fd6d8d8556c98d5bcd42548
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-02-27 01:01:54 -0400

    test example to rlsp/users/create_test_exs

---
+++ test/rlsppay/users/create_test.exs
@@ -0,0 +1,47 @@
+defmodule Rlsppay.Users.CreateTest do
+  use Rlsppay.DataCase
+
+  alias Rlsppay.User
+  alias Rlsppay.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, returns an user" do
+      params = %{
+        name: "Rodrigo",
+        password: "1234567",
+        nickname: "rlsp",
+        email: "rlsp@gmail.com",
+        age: 40
+      }
+
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+      user = Repo.get(User, user_id)
+
+      assert %User{name: "Rodrigo", age: 40, id: ^user_id} = user
+
+    end
+
+    test "when there are invalid params, returns an user" do
+      params = %{
+        name: "Rodrigo",
+        nickname: "rlsp",
+        email: "rlsp@gmail.com",
+        age: 17
+      }
+
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["can be blank"]
+      }
+
+      assert errors_on(changeset) == expected_response
+
+    end
+
+  end
+
+end
